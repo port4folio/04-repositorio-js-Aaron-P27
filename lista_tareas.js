dfab9494f9c63434d344ac5c8cd162b2fac8e0da
Aaron <poncedonosoa@gmail.com>
2026-05-11 11:18:46 -0400

    commit1105

--- lista_tareas.js
+++ lista_tareas.js
@@ -12,3 +12,37 @@ function listarTareas(t) {
       li.className = "list-group-item";
     });
 }
+
+let btnAgregar = document.getElementById("btnAgregar"); //captura el boton
+btnAgregar.addEventListener("click", agregarTarea); //agrega el evento click
+
+function agregarTarea() {
+  //funcion para agregar tarea
+  let tarea = document.getElementById("txtTarea").value; //captura el valor del input tarea
+  tareas.push(tarea); //agrega la tarea al arreglo de tareas
+  listarTareas(tareas) //llama a la funcion listarTareas para mostrarlas
+}
+
+let btnBuscar = document.getElementById("btnBuscar"); //captura boton
+btnBuscar.addEventListener("click", buscarTarea);
+
+function buscarTarea() {
+  let tareaBuscada = document.getElementById("txtTarea").value;
+
+  if (tareaBuscada == "") {
+    listarTareas(tareas);
+  } else {
+    tareasEncontradas = tareas.filter((tarea) => tarea == tareaBuscada);
+
+      if (tareasEncontradas.length > 0) {
+        listarTareas(tareasEncontradas);
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops",
+          text: "No se encontraron tareas",
+          footer: ""
+      });
+    }
+  }
+}
